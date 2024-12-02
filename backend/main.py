99c734000ee54212bccf5ac4a17bea95cd4eba08
Dominik Farlik <xfarlik@mendelu.cz>
2024-12-02 22:32:39 +0100

    Deleted old inefficent code

--- backend/main.py
+++ backend/main.py
@@ -11,19 +11,3 @@ if __name__ == "__main__":
     print("Removed clause negations: " + print_formula(formula))
     formula.distribute_disjunctions()
     print("Disjunction distribution: " + print_formula(formula))
-
-    #formula1 = Formula(input_formula)
-    #print("First split cycle:\n" + str(formula1) + "\n")
-    #formula1.split_to_clauses_2nd_cycle()
-    #print("Second split cycle:\n" + str(formula1) + "\n")
-    #formula1.remove_implications_and_equivalences()  # TODO: finish  func
-    #print("Removed implications:\n" + str(formula1) + "\n")
-    #formula1.convert_to_NNF()  # TODO: finish  func
-    #print("Converted to NNF:\n" + str(formula1) + "\n")
-    #formula1.disjunction_distribution()  # TODO: finish  func
-    #print("Disjunction distribution:\n" + str(formula1) + "\n")
-    #if formula1.is_formula_in_cnf():  # TODO: finish  func
-    #    formula1.negate_formula_for_resolution()  # TODO: finish  func
-    #    # print("Negate each clause for resolution:\n" + str(formula1) + "\n")
-    #    print("Resolution")
-    #    formula1.resolute_formula()  # TODO: finish  func
