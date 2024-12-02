b9d02aa6679fd332a8eece4ba2c2c49df8647e28
DominikFarlik <153185929+DominikFarlik@users.noreply.github.com>
2024-12-02 16:41:59 +0100

    Moved old solution to another file, finished new func for removing implications

--- backend/Clause.py
+++ backend/Clause.py
@@ -1,9 +1,18 @@
-from main import Operator
+from enum import Enum
+
+
+class Operator(Enum):
+    NOT = "¬"
+    AND = "∧"
+    OR = "∨"
+    IMPLICATION = "→"
+    EQUIVALENCE = "↔"
+
 
 class Clause:
     def __init__(self, parent=None):
         self.clause = []
-        self.parent =  self if parent is None else parent
+        self.parent = self if parent is None else parent
 
     def __str__(self):
         return str(self.clause)
@@ -19,8 +28,33 @@ class Clause:
         return self.parent
 
     def remove_implication(self):
-        for clause in self.clause:
+        clauses = self.clause.copy()
+        for index, clause in enumerate(clauses):
             if isinstance(clause, Clause):
                 clause.remove_implication()
             elif clause == Operator.IMPLICATION.value:
-                print(self.clause)
+                self.clause[index] = Operator.OR.value
+                self.clause.insert(index - 1, Operator.NOT.value)
+
+
+def parse_formula(input_formula):
+    formula = Clause()
+    current_clause = formula
+    for char in input_formula:
+        if char == "(":
+            current_clause = current_clause.add_clause(current_clause)
+        elif char == ")":
+            current_clause = current_clause.get_parent()
+        else:
+            current_clause.add_literal(char)
+    return formula
+
+
+def print_formula(formula):
+    out_formula = ""
+    for clause in formula.clause:
+        if isinstance(clause, Clause):
+            out_formula += "(" + str(print_formula(clause)) + ")"
+        else:
+            out_formula += clause
+    return out_formula
